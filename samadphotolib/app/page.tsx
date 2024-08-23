2b72884ff2d4ae54a8f27ced528279994adcbd14
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-08-23 12:18:31 +0500

    1218

--- samadphotolib/app/page.tsx
+++ samadphotolib/app/page.tsx
@@ -3,21 +3,39 @@
 import Image from "next/image";
 import { CldUploadButton } from "next-cloudinary";
 
+import { CldImage } from "next-cloudinary";
+
+type uploadResult = {
+  info : {
+    public_id: string;
+  }
+  events: "success"
+}
+
 export default function Home() {
+
+const []
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-       <CldUploadButton uploadPreset="ml_default" />   
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
+       <CldUploadButton 
+       onUpload={(result: uploadResult) => {
+         console.log(result.info.public_id);
+       }}
+       uploadPreset="la9hjhns" 
+       /> 
+
+      <CldImage
+      width="960"
+      height="600"
+      src="<Public ID>"
+      sizes="100vw"
+      alt="Description of my image"
+      />
+
+
+
+
        </main>
   );
 }
